561035d0d533af45154b9f40d677db5c2611e185
zorro.li <zorro.li@sunvalley.com.cn>
2017-11-10 13:53:54 +0800

    first commit

---
+++ src/main/java/com/zero/testdemo1/HelloController.java
@@ -0,0 +1,8 @@
+package com.zero.testdemo1;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+}
---
+++ src/main/java/com/zero/testdemo1/TestDemo1Application.java
@@ -0,0 +1,12 @@
+package com.zero.testdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDemo1Application.class, args);
+	}
+}
